7b77dbd32d670e5b717404564d5652a79dd1a5a1
Santino Majur <demalong22@gmail.com>
2024-07-20 01:14:42 +0200

    Added title and description to slides

--- app/components/Carousel.tsx
+++ app/components/Carousel.tsx
@@ -23,7 +23,7 @@ const slideStyles = {
 const slideContainerStyles = {
   display: "flex",
   height: "100%",
-  transition: "transform ease-out 0.3s",
+  transition: "transform ease-out 0.2s",
 };
 
 const slideContainerOverflowStyles = {
@@ -72,30 +72,44 @@ const Carousel: React.FC<CarouselProps> = ({ slides, parentWidth }) => {
   }, [nextSlide]);
   return (
     <div className="h-full relative ">
-      <div className=" absolute z-30 w-full h-full bg-black/50 group ">
-        <div
-          style={{ transform: "translate(0, -50%)" }}
-          className="hidden group-hover:flex justify-center items-center absolute top-[50%] rounded-full bg-white/50 hover:bg-gray-800 hover:text-gray-300 hover:ring-2 left-8 md:left-12 lg:left-24 text-5xl text-black z-10 cursor-pointer "
-          onClick={prevSlide}
-        >
-          <BsChevronCompactLeft />
-        </div>
-        <div
-          style={{ transform: "translate(0, -50%)" }}
-          className="hidden group-hover:flex justify-center items-center absolute top-[50%]  rounded-full bg-white/50 hover:bg-gray-800 hover:text-gray-300 hover:ring-2 right-8 md:right-12 lg:right-24 text-5xl text-black  z-10 cursor-pointer "
-          onClick={nextSlide}
-        >
-          <BsChevronCompactRight />
-        </div>
+      <div
+        style={{ transform: "translate(0, -50%)" }}
+        className=" absolute top-[50%] rounded-full bg-white/50 hover:bg-gray-800 hover:text-gray-300 hover:ring-2 left-8 md:left-12 lg:left-24 text-5xl text-black z-10 cursor-pointer "
+        onClick={prevSlide}
+      >
+        <BsChevronCompactLeft />
+      </div>
+      <div
+        style={{ transform: "translate(0, -50%)" }}
+        className=" absolute top-[50%]  rounded-full bg-white/50 hover:bg-gray-800 hover:text-gray-300 hover:ring-2 right-8 md:right-12 lg:right-24 text-5xl text-black  z-10 cursor-pointer "
+        onClick={nextSlide}
+      >
+        <BsChevronCompactRight />
       </div>
 
       <div style={slideContainerOverflowStyles}>
+        <div className="absolute w-full h-full top-0 right-0 left-0 bg-black/50"></div>
         <div style={getSlideContainerStylesWithWidth()}>
           {slides.map((_: any, slideIndex: any) => (
             <div
               key={slideIndex}
               style={getSlideStylesWithBackground(slideIndex)}
-            ></div>
+              className="relative"
+            >
+              {slideIndex === currIndex && (
+                <div className="absolute top-24 left-16 md:left-28 lg:left-40 flex flex-col justify-center gap-4 w-[25%] md:w-[30%] lg:w-[40%] p-4 bg-black/50 text-white z-50 rounded-lg">
+                  <h2 className="text-4xl">{slides[slideIndex].title}</h2>
+                  <p className="">{slides[slideIndex].description}</p>
+                  <hr />
+                  <Link
+                    href={"#"}
+                    className="px-8 py-2 w-40 rounded-md bg-teal-500 text-white font-bold transition duration-200 hover:bg-white hover:text-black border-2 border-transparent hover:border-teal-500"
+                  >
+                    Learn More
+                  </Link>
+                </div>
+              )}
+            </div>
           ))}
         </div>
       </div>
--- app/data.js
+++ app/data.js
@@ -1,6 +1,26 @@
 export const slides = [
-  { title: "image1", url: "https://i.imgur.com/Gu5Cznz.jpg" },
-  { title: "image2", url: "https://i.imgur.com/idjXzVQ.jpg" },
-  { title: "image3", url: "https://i.imgur.com/8DYumaY.jpg" },
-  { title: "image4", url: "https://i.imgur.com/8IuucQZ.jpg" },
+  {
+    title: "Agriculture",
+    url: "https://i.imgur.com/Gu5Cznz.jpg",
+    description:
+      "Agriculture is the practice of cultivating soil, raising livestock, and producing crops. It also includes the preparation and marketing of the resulting products.",
+  },
+  {
+    title: "Construction",
+    url: "https://i.imgur.com/idjXzVQ.jpg",
+    description:
+      "Construction is a general term meaning the art and science of forming objects, systems, or organizations. It comes from the Latin word constructio and Old",
+  },
+  {
+    title: "Transportation",
+    url: "https://i.imgur.com/8DYumaY.jpg",
+    description:
+      "transportation, the movement of goods and persons from place to place and the various means by which such movement is accomplished.",
+  },
+  {
+    title: "Water Drilling",
+    url: "https://i.imgur.com/8IuucQZ.jpg",
+    description:
+      "Well drilling is the process of drilling a hole in the ground for the extraction of a natural resource such as ground water, brine, natural gas",
+  },
 ];
